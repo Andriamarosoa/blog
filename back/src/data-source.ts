f3ae5332f3e3acde26042315fdfc18893a24430d
Andriamarosoa <tinah.andriamarosoa@gmail.com>
2025-03-25 18:38:56 +0400

    adding docker compose file

--- back/src/data-source.ts
+++ back/src/data-source.ts
@@ -1,12 +1,21 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+dotenv.config()
+
+
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_PORT = process.env.DB_PORT!;
+
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root", // Mets ton utilisateur MySQL
-  password: "root", // Mets ton mot de passe
+  host: DB_HOST,
+  port:+DB_PORT,
+  username: DB_USER, // Mets ton utilisateur MySQL
+  password: DB_PASSWORD, // Mets ton mot de passe
   database: "blog_db",
   entities: ["src/entities/*.ts"], // Dossier où sont stockées tes entités
   synchronize: true, // Synchronise la BDD à chaque démarrage (Désactive en prod)
